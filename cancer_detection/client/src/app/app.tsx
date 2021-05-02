f79f3cb382d11963d8c1cab14418802811e97216
Reshav Abraham <reshavabraham@gmail.com>
2021-05-02 13:58:41 -0400

    typescript react template

--- cancer_detection/client/src/app/app.tsx
+++ cancer_detection/client/src/app/app.tsx
@@ -1,2 +1,6 @@
 // https://github.com/TypeStrong/ts-loader/issues/595
-document!.getElementById('root')!.textContent = 'hello World';
+// document!.getElementById('root')!.textContent = 'hello World';
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(<div>Hello</div>, document.getElementById('root'))
